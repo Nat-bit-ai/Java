f9dca21c991dfe022111dd61faa17dd8b1ba6b50
Natnael Zerihun <nathyzer21@gmail.com>
2026-05-26 21:26:43 +0300

    ass=01

--- Vehicle.java
+++ Vehicle.java
@@ -1,5 +1,13 @@
 package main;
 
 public class Vehicle {
-
+	int no_of_seats;
+	int no_of_wheels;
+	
+	Vehicle(int no_of_seats,int no_of_wheels){
+		this.no_of_seats =no_of_seats;
+		this.no_of_wheels=no_of_wheels;
+	}
 }
+
+
