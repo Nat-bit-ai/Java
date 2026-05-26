41bb16607420fd76ee9f5dc9843daf50cbce79bd
Natnael Zerihun <nathyzer21@gmail.com>
2026-05-26 21:39:45 +0300

    ass-02

---
+++ Leap.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Leap {
+	Boolean isLeapYear(int year) {
+		if(year % 400 == 0) {
+			return true;
+		}
+		else if (year % 100 == 0) {
+			return false;
+		}
+		else if (year % 4 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 package main;
-import main.Vehicle;
+import main.Leap;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Vehicle motor = new Vehicle(2,2);
-		Vehicle car = new Vehicle(4,4);
-		System.out.println("motor has " + motor.no_of_seats + " num of seats and " + motor.no_of_wheels + " num of wheels" );
-		System.out.println("car has " + car.no_of_seats + " num of seats and " + car.no_of_wheels + " num of wheels" );
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Year : ");
+		int year = input.nextInt();
+		Leap test = new Leap();
+		boolean result = test.isLeapYear(year);
+		System.out.println(result);
+        input.close();
+		
 	}
-
 }
